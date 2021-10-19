a8fbb94b5410a2b7c443cca1709e589bb762e8a8
cshepscorp <sheppard.christy@gmail.com>
2021-10-19 14:06:07 -0400

    dynamically generated HTML page and refactored

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js'); // variable name here can be whatever but the relative path must be precise
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-  // This...
-  // for (let i = 0; i < profileDataArr.length; i += 1) {
-  //  console.log(profileDataArr[i]);
-  //  }
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
+const profileDataArgs = process.argv.slice(2);
 
-  console.log('================');
+const [name, github] = profileDataArgs;
 
-  // Is the same as this...
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-  });
-};
+//console.log(generatePage(name, github));
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+	if (err) throw err;
+	console.log('Portfolio complete! Check out index.html to see the output!');
+});
+ 
+
